7b18317f2cc5ac8136022114134f6cf4845de29e
Andrii Shtadler <andrishtadler@gmail.com>
2019-01-26 10:04:45 +0200

    Added example with fetch and concater

--- src/form/concater.js
+++ src/form/concater.js
@@ -3,9 +3,19 @@ export default class Concater {
     this.form = form;
   }
 
+  // get concat name and surname from form if it user form
   getFullName() {
-    const name = this.form.elements.name.value;
-    const surname = this.form.elements.surname.value;
-    console.log(name+surname);
+    if(this.someChecker()) {
+      const name = this.form.elements.name.value;
+      const surname = this.form.elements.surname.value;
+      console.log(name + surname);
+    } else {
+      console.log('not user form, sorry :(');
+    }
+  }
+
+  // some action with error, debug fixing
+  someChecker() {
+    return this.form.classList.value.indexOf('user-form');
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import Concater from './form/concater';
 import './index.scss';
 
+// create new instance of concater with form link
 const concater = new Concater(document.forms[0]);
 
 const button = document.querySelector('.button-ok');
-button.addEventListener('click', concater.getFullName);
+// some action on click
+button.addEventListener('click', () => concater.getFullName());
+
+
+// async breakpoint
+// XHR/fetch breakpoints, and manual breakpoint in callback
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => console.log(json));
